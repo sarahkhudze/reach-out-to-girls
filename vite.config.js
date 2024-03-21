0356fedaef4a446c0196ba6aeca6f56ff9daca30
coachemmy <coachemmyb@gmail.com>
2024-03-21 23:39:42 +0100

    identiko version 1.2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import reactRefreach from '@vitejs/plugin-react-refreach';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [
     react(),
-    reactRefreach(),   
+    reactRefresh(),   
     autoprefixer(),
     tailwindcss('./tailwind.config.js'),
   ],
