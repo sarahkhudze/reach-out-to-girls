c6ef0579ca505e5e7caf486ec674a2c24356626f
coachemmy <coachemmyb@gmail.com>
2024-05-30 10:52:56 +0100

    enhancements

--- src/components/about/PreCTAAbout.jsx
+++ src/components/about/PreCTAAbout.jsx
@@ -12,11 +12,11 @@ const PreCTAAbout = () => {
         >
             <div className="max-w-screen-xl mx-auto grid md:grid-cols-1 gap-y-10 md:gap-y-12 lg:gap-y-14 xl:gap-y-16 text-center px-4 md:px-8 lg:px-12 xl:px-16">
                 <div className="flex flex-col items-center justify-center text-[#ffffff] z-10">
-                    <h2 className="font-bold text-[150%] md:text-[300%] mt-4 md:mt-0 mb-2 shadow">
+                    <h2 className="font-bold text-[150%] md:text-[300%] mt-4 md:mt-0 mb-2 text-shadow-lg">
                         Pioneering Biometric Excellence For  <br />
                         Unparalleled Data Protection
                     </h2>
-                    <p className="text-xs md:text-xl mb-[4%] font-bold bg:shadow">
+                    <p className="text-xs md:text-xl mb-[4%] font-bold text-shadow-lg">
                         Identiko specializes in identity management technology, verification,
                         and digital identity solutions.
                     </p>
