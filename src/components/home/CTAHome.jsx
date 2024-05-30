71b2dcffade40603bb0fac6351dae3e421864293
coachemmy <coachemmyb@gmail.com>
2024-05-30 13:48:27 +0100

    enhancements

--- src/components/home/CTAHome.jsx
+++ src/components/home/CTAHome.jsx
@@ -8,9 +8,9 @@ const CTAHome = () => {
     return (
 
         <div className="h-screen overflow-hidden relative">
-            <video className="h-40 md:h-auto w-screen min-w-full min-h-full relative m-0 p-0" src={LandingVid} autoPlay loop muted alt="img1" />
+            <video className="h-40 md:h-auto bg:cover bg:center min-w-full min-h-full relative m-0 p-0" src={LandingVid} autoPlay loop muted alt="img1" />
 
-            <div className=" absolute md:z-50 font-bold text-[50%] md:text-[100%] mt-32 text-white w-full h-full md:top-0 flex items-center justify-center">
+            <div className=" absolute md:z-50 text-[50%] md:text-[100%] mt-32 text-white w-full h-full md:top-0 flex items-center justify-center">
                 <p className="text-center text-shadow-lg"> Identiko Is Your One-Stop Shop <br />
                                                             For Digital Identity, Verification,<br />
                                                              And Technology Solutions</p>
