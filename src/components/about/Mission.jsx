657f552f8d9bd6077e80279b1535764bcfec772b
Ayomide <oyetimehin31@gmail.com>
2024-07-22 10:18:50 +0100

    changes primary pillars to core values

--- src/components/about/Mission.jsx
+++ src/components/about/Mission.jsx
@@ -42,9 +42,10 @@ const Mission = () => {
             <div className="relative max-w-screen-xl mx-auto mt-4 md:mt-0 px-4 md:px-8 lg:px-12 xl:px-16 flex flex-col justify-center items-center gap-y-10 text-center z-50">
 
                 <h2 className="font-semibold text-xl md:text-3xl md:mb-2 mt-0">
-                    Our Primary Pillars
+                    Our Core Values
                 </h2>
 
+<<<<<<< Updated upstream
                 <p className="md:w-[40%] text-sm md:text-sm pr-4 md:pr-0">
                     Identiko specializes in identity management technology, verification,
                     and digital identity solutions. Our ICT professionals are experts in
@@ -72,6 +73,35 @@ const Mission = () => {
                             </p>
                         </div>
                     ))}
+=======
+                <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-[6%] md:gap-[10%] md:mt-8 transform animate-slide-up">
+                    <div className={`bg-[#707070] text-white hover:scale-110 rounded-xl overflow-hidden border shadow-md p-16 md:p-8'
+                        }`} >
+                        <p className="text-md md:text-xl -mt-2 md:mt-8  font-bold">Quality Solutions</p>
+                        <p className="text-xs md:text-sm text-justify mt-2 md:mt-4 flex items-center justify-between">Reliability <span><MdCheckCircleOutline /></span></p>
+                        <p className="text-xs md:text-sm text-justify mt-2 md:mt-4 flex items-center justify-between">Availability<span><MdCheckCircleOutline /></span></p>
+                        <p className="text-xs md:text-sm text-justify mt-2 md:mt-4 flex items-center justify-between">Serviceability<span><MdCheckCircleOutline /></span></p>
+                        <p className="text-xs md:text-sm text-justify mb-16 md:mb-0 mt-2 md:mt-4 flex items-center justify-between">Durability<span><MdCheckCircleOutline /></span></p>
+                    </div>
+
+                    <div className={`bg-[#014C98] text-white hover:scale-110 duration-500 rounded-xl overflow-hidden shadow-xl p-12 md:p-8 border'
+                        }`} >
+                        <p className="text-md md:text-xl -mt-2 md:mt-8  font-bold">Quality Delivery</p>
+                        <p className="text-xs md:text-sm text-justify mt-2 md:mt-4 flex items-center justify-between">On-time Delivery <span><MdCheckCircleOutline /></span></p>
+                        <p className="text-xs md:text-sm text-justify mt-2 md:mt-4 flex items-center justify-between">Seamless<span><MdCheckCircleOutline /></span></p>
+                        <p className="text-xs md:text-sm text-justify mt-2 md:mt-4 flex items-center justify-between">Secure<span><MdCheckCircleOutline /></span></p>
+                        <p className="text-xs md:text-sm text-justify  mb-16 md:mb-0 mt-2 md:mt-4 flex items-center justify-between">Extensive Support<span><MdCheckCircleOutline /></span></p>
+                    </div>
+
+                    <div className={`bg-[#0F90CA]  text-white hover:scale-110 duration-500 rounded-xl overflow-hidden shadow-md p-12 md:p-8 border'
+                        }`} >
+                        <p className="text-md md:text-xl -mt-2 md:mt-8 font-bold">Quality Results</p>
+                        <p className="text-xs md:text-sm text-justify mt-2 md:mt-4 flex items-center justify-between">Measurable Results <span><MdCheckCircleOutline /></span></p>
+                        <p className="text-xs md:text-sm text-justify mt-2 md:mt-4 flex items-center justify-between">Optimization<span><MdCheckCircleOutline /></span></p>
+                        <p className="text-xs md:text-sm text-justify mt-2 md:mt-4 flex items-center justify-between">Satisfaction<span><MdCheckCircleOutline /></span></p>
+                        <p className="text-xs md:text-sm text-justify  mb-16 md:mb-0 mt-2 md:mt-4 flex items-center justify-between">Innovation<span><MdCheckCircleOutline /></span></p>
+                    </div>
+>>>>>>> Stashed changes
                 </div>
             </div>
             <style>{`@keyframes slide-up{ from{ transform: translateY(100%); opacity: 0 } to{ transform: translateY(0%); opacity: 1 }} .animate-slide-up {animation: slide-up 2s ease-out;`} </style>
