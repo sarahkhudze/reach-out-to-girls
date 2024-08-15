307b66f5c0af653379e22d0ed80c94cbc519f3f4
Coachemmy <coachemmyb@gmail.com>
2024-08-15 15:45:31 -0700

    two maps on about

--- src/components/others/Map.jsx
+++ src/components/others/Map.jsx
@@ -1,6 +1,6 @@
 const Map = () => {
   return (
-    <div className="flex flex-col md:flex-row gap-2">
+    <div className="grid grid-cols-1 grid-cols-2 gap-4">
       <div className="relative w-1/2 h-screen">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.188474154057!2d3.3569781741859823!3d6.6234955933707305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b931c7d760241%3A0x2574c9d09723d3f1!2sIdentiko+Integrated+Solutions!5e0!3m2!1sen!2sng!4v1717106108351!5m2!1sen!2sng"
