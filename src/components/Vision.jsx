837c53ff474f4395156a1238b13a3817a373c987
CoachEmmy <coachemmyb@gmail.com>
2025-10-28 22:18:00 +0800

    uiux touches on home, about and privacy

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -3,7 +3,7 @@ import Cards from "./Cards";
 const Vision = () => {
   return (
     <div className="relative bg-white overflow-x-hidden bg-cover bg-center grid md:py-20 md:px-8 p-8 border-0">
-      <div className="absolute top-0 left-0 w-full h-full bg-black"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-[#092b28]"></div>
       <Cards />
     </div>
   );
